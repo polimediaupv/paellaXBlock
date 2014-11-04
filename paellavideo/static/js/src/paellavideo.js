a6ff57e31e8b37153f779dc2af0ae02d46e3a188
leosamu <leosamu@gmail.com>
2014-11-04 12:36:08 +0000

    fixed bug with multiple videos in same page

--- paellavideo/static/js/src/paellavideo.js
+++ paellavideo/static/js/src/paellavideo.js
@@ -6,8 +6,8 @@ function paellaXBlock(runtime, element) {
 
     $($("iframe",element)[0]).on('load',function(){
         setTimeout(function(){
-            start = $("[name='trimstart']")[0].value;
-            end = $("[name='trimend']")[0].value;
+            start = $("[name='trimstart']",element)[0].value;
+            end = $("[name='trimend']",element)[0].value;
             if (start < end && end>0){
                 $("iframe",element)[0].contentWindow.postMessage({event:'paella:settrim',params:{trimEnabled:true,trimStart:start,trimEnd:end}},"http://paellaplayer.upv.es");
             }
